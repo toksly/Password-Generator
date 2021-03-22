5903e892749c7c9f5386d392f45a0cf0d69b9ca7
toksly <nwizutoks@gmail.com>
2021-03-21 19:52:48 -0500

    updated script.js file

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 // Array of special characters to be included in password
 var specialCharacters = [
   '@',
